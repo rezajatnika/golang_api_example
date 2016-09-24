eecb47fd4800a72343e99c171b3f2cdd2e3bef60
Reza Jatnika <rezajatnika@gmail.com>
2016-09-24 09:41:02 +0700

    Add Post and Delete User handler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	// Standard library packages
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -18,14 +19,6 @@ type (
 		Age  int    `json:"age"`
 	}
 )
-
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "Hello, world!\n")
-}
-
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "Hello, %s!\n", ps.ByName("name"))
-}
 
 func UserShow(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	// Stub an example user resource
@@ -44,16 +37,42 @@ func UserShow(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "%s\n", uj)
 }
 
+func UserPost(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Stub an example user to be populated from the body
+	user := User{}
+
+	// Populate the user data
+	json.NewDecoder(r.Body).Decode(&user)
+
+	// Add user id
+	user.Id = "1234"
+
+	// Return user JSON encoding
+	uj, _ := json.Marshal(user)
+
+	// Render as JSON with header
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func UserDelete(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// TODO: Only write status for now
+	w.WriteHeader(200)
+}
+
 func main() {
 	// Instantiate a new router
 	router := httprouter.New()
 
-	// Example of Hello, world!
-	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
-
 	// Get a user resource
 	router.GET("/users/:id", UserShow)
+
+	// Post user resource
+	router.POST("/users", UserPost)
+
+	// Delete a user resource
+	router.DELETE("/users/:id", UserDelete)
 
 	// Run http server
 	http.ListenAndServe("127.0.0.1:8080", router)
