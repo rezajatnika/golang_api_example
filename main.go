b50835bbb33bad6338434373eca374480b0a60d6
Reza Jatnika <rezajatnika@gmail.com>
2016-09-24 07:50:35 +0700

    Add Hello handler ("/hello/:name")

--- main.go
+++ main.go
@@ -12,8 +12,14 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Hello, world!")
 }
 
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "Hello, %s!\n", ps.ByName("name"))
+}
+
+
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
 	http.ListenAndServe("127.0.0.1:8080", router)
 }
