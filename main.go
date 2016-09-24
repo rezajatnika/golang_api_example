866049cff0124d3e168ac335104bf18938a01e86
Reza Jatnika <rezajatnika@gmail.com>
2016-09-24 07:45:57 +0700

    Hello, world using httprouter package

--- main.go
+++ main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	// Third-party packages
+	"github.com/julienschmidt/httprouter"
 )
 
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Hello, world!")
 }
 
 func main() {
-	http.HandleFunc("/", IndexHandler)
-	http.ListenAndServe("127.0.0.1:8080", nil)
+	router := httprouter.New()
+	router.GET("/", Index)
+	http.ListenAndServe("127.0.0.1:8080", router)
 }
